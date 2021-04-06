446f198d04dd6f7d141af98c2e46451e7baa8eb7
SneyderHOL <1507@holbertonschool.com>
2021-04-06 16:20:18 -0500

    Check if article is valid

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it "tests article object" do
+  it "tests that article is valid" do
     article = create(:article)
-    expect(article.title).to eq('Sample article')
+    expect(article).to be_valid #article.valid? == true
   end
 end
