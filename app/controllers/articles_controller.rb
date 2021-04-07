f04d4f10830a09151d21afc27cb22a753003d5e2
SneyderHOL <1507@holbertonschool.com>
2021-04-07 10:11:12 -0500

    Add jsonapi-serializer gem and tests for get method articles endpoint

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,13 @@
 class ArticlesController < ApplicationController
   def index
+    articles = Article.all
+    render json: serializer.new(articles), status: :ok
   end
 
   def show
+  end
+
+  def serializer
+    ArticleSerializer
   end
 end
