a81a79e91ee961eaa91e1e6146e965bff272a639
SneyderHOL <1507@holbertonschool.com>
2021-04-22 16:56:22 -0500

    Update tests to manage standard login authentication feature

--- app/lib/user_authenticator.rb
+++ app/lib/user_authenticator.rb
@@ -1,5 +1,5 @@
 class UserAuthenticator
-  attr_reader :authenticator
+  attr_reader :authenticator, :access_token
 
   def initialize(code: nil, login: nil, password: nil)
     @authenticator = if code.present?
@@ -11,41 +11,20 @@ class UserAuthenticator
 
   def perform
     authenticator.perform
+    set_access_token
   end
 
   def user
     authenticator.user
   end
 
-  def access_token
-    authenticator.access_token
-  end
-
   private
 
-  def client
-    @client ||= Octokit::Client.new(
-      client_id: ENV['GITHUB_CLIENT_ID'],
-      client_secret: ENV['GITHUB_CLIENT_SECRET']
-    )
-  end
-  
-  def token
-    @token ||= client.exchange_code_for_token(code)
-  end
-
-  def user_data
-    @user_data ||= Octokit::Client.new(access_token: token)
-      .user.to_h.slice(:login, :avatar_url, :url, :name)
-  end
-
-  def prepare_user
-    @user = if User.exists?(login: user_data[:login])
-      User.find_by(login:user_data[:login])
+  def set_access_token
+    @access_token = if user.access_token.present?
+      user.access_token
     else
-      User.create(user_data.merge(provider: 'github'))
+      user.create_access_token
     end
   end
-
-  attr_reader :code
 end
